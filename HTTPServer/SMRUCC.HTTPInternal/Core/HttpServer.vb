a4cb13d824e6dd51aa2e785abdd253c1bbfd7417
xieguigang <xie.guigang@gmail.com>
2016-08-10 22:07:59 +0800

    threadpool

--- HTTPServer/SMRUCC.HTTPInternal/Core/HttpServer.vb
+++ HTTPServer/SMRUCC.HTTPInternal/Core/HttpServer.vb
@@ -1,27 +1,27 @@
 ﻿#Region "Microsoft.VisualBasic::62e91f5abc3bea9184ad44449d43a4c0, ..\httpd\HTTPServer\SMRUCC.HTTPInternal\Core\HttpServer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -32,6 +32,7 @@ Imports System.Net.Sockets
 Imports System.Threading
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Parallel
+Imports Microsoft.VisualBasic.Parallel.Linq
 
 Namespace Core
 
@@ -72,7 +73,7 @@ Namespace Core
             Me._httpListener = New TcpListener(IPAddress.Any, _LocalPort)
         End Sub
 
-        Dim threadPool As New Threads.ThreadPool
+        Dim threadPool As New Threads.ThreadPool(LQuerySchedule.Recommended_NUM_THREADS * 8)
 
         ''' <summary>
         ''' Running this http server. 
