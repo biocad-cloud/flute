6180ca1e012a95268510b6e00d8976338eafb2f5
xieguigang <xie.guigang@gmail.com>
2016-09-03 20:15:10 +0800

    execute create table sql

--- SMRUCC.WebCloud.GIS/MaxMind/MySqlImports.vb
+++ SMRUCC.WebCloud.GIS/MaxMind/MySqlImports.vb
@@ -2,6 +2,7 @@
 Imports System.Text
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.DocumentStream.Linq
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.Extensions
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Linq
 Imports Oracle.LinuxCompatibility.MySQL
@@ -21,12 +22,16 @@ Namespace MaxMind
         <Extension>
         Public Function ImportsGeoLite2CountryBlocksIPv4(mysql As MySQL, df As String) As Boolean
             Dim data As geolite2_country_blocks_ipv4() = df.LoadCsv(Of geolite2_country_blocks_ipv4)
+            Dim SQL As New Value(Of String)
 
             If data.IsNullOrEmpty Then
                 Return False
             End If
 
             Call mysql.Execute(DropTableSQL(Of geolite2_country_blocks_ipv4))
+            If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of geolite2_country_blocks_ipv4)()) Then
+                Call mysql.Execute(SQL)
+            End If
 
             For Each x As geolite2_country_blocks_ipv4 In data
                 Call mysql.ExecInsert(x)
@@ -44,9 +49,15 @@ Namespace MaxMind
         ''' 
         <Extension>
         Public Function ImportsGeoLite2CountryBlocksIPv6(mysql As MySQL, df As String) As Boolean
+            Dim SQL As New Value(Of String)
+
             Using reader As New DataStream(df,, 1024 * 1024 * 10)
                 Call mysql.Execute(DropTableSQL(Of geolite2_country_blocks_ipv6))
-                Call reader.ForEach(Of geolite2_country_blocks_ipv6)(Sub(x) Call mysql.ExecInsert(x))
+                If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of geolite2_country_blocks_ipv6)()) Then
+                    Call mysql.Execute(SQL)
+                End If
+
+                Call reader.ForEach(Of geolite2_country_blocks_ipv6)(AddressOf mysql.ExecInsert)
             End Using
 
             Return True
@@ -54,7 +65,12 @@ Namespace MaxMind
 
         <Extension>
         Public Function ImportsGeoLite2CountryLocations(mysql As MySQL, DIR As String) As Boolean
+            Dim SQL As New Value(Of String)
+
             Call mysql.Execute(DropTableSQL(Of geolite2_country_locations))
+            If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of geolite2_country_locations)()) Then
+                Call mysql.Execute(SQL)
+            End If
 
             For Each df As String In ls - l - r - wildcards("GeoLite2-Country-Locations*.csv") <= DIR
                 Dim data = df.LoadCsv(Of geolite2_country_locations)
