aba797b5337acddbae4ba3f6152bcbc6f9be3122
xieguigang <xie.guigang@gmail.com>
2016-09-03 09:11:26 +0800

    fix namespace errors

---
+++ SMRUCC.WebCloud.GIS/MaxMind/Engine.vb
@@ -0,0 +1,64 @@
+﻿Imports Microsoft.VisualBasic.Net
+Imports Oracle.LinuxCompatibility.MySQL
+Imports SMRUCC.WebCloud.GIS.MaxMind.geolite2
+
+Namespace MaxMind
+
+    ''' <summary>
+    ''' 地理位置服务查询引擎
+    ''' </summary>
+    Public Class Engine
+
+        Public ReadOnly Property MySQl As MySQL
+
+        Sub New(uri As ConnectionUri)
+            Me.MySQl = uri
+        End Sub
+
+        Private ReadOnly Mask As String() = {"255.0.0.0", "255.255.0.0", "255.255.255.0"}
+
+        Public Function LocationQuery(IPAddress As String) As Database.FindResult
+
+            If String.IsNullOrEmpty(IPAddress) OrElse Not Net.IPAddress.TryParse(IPAddress, Nothing) Then
+                Return Database.FindResult.Null
+            End If
+
+
+            Dim CIDR As String() = (From mask As String
+                                In Me.Mask
+                                    Select value = New IPv4(IPAddress, mask).CIDR).ToArray
+            Dim LQuery = (From s_cidr As String In CIDR
+                          Let Query = MySQl.ExecuteScalar(Of geolite2_city_blocks_ipv4)($"SELECT * FROM geoip_services.geolite2_city_blocks_ipv4 where network = '{s_cidr}';")
+                          Where Not Query Is Nothing
+                          Select Query).FirstOrDefault
+            If LQuery Is Nothing Then
+                Return Database.FindResult.Null
+            End If
+
+            Dim CityLocation = MySQl.ExecuteScalar(Of geolite2_city_locations)($"SELECT * FROM geoip_services.geolite2_city_locations where geoname_id = '{LQuery.geoname_id}';")
+
+            If CityLocation Is Nothing Then
+                CityLocation = New geolite2_city_locations
+            End If
+
+            Return New Database.FindResult With {
+            .CIDR = LQuery.network,
+            .city_name = CityLocation.city_name,
+            .continent_code = CityLocation.continent_code,
+            .continent_name = CityLocation.continent_name,
+            .country_iso_code = CityLocation.country_iso_code,
+            .country_name = CityLocation.country_name,
+            .geoname_id = CityLocation.geoname_id,
+            .latitude = LQuery.latitude,
+            .longitude = LQuery.longitude,
+            .metro_code = CityLocation.metro_code,
+            .postal_code = LQuery.postal_code,
+            .subdivision_1_iso_code = CityLocation.subdivision_1_iso_code,
+            .subdivision_1_name = CityLocation.subdivision_1_name,
+            .subdivision_2_iso_code = CityLocation.subdivision_2_iso_code,
+            .subdivision_2_name = CityLocation.subdivision_2_name,
+            .time_zone = CityLocation.time_zone
+        }
+        End Function
+    End Class
+End Namespace
---
+++ SMRUCC.WebCloud.GIS/MaxMind/Views.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Net
+
+Namespace MaxMind.Views
+
+    Public Class Country
+        <Xml.Serialization.XmlAttribute> Public Property continent_code As String
+        <Xml.Serialization.XmlAttribute> Public Property continent_name As String
+        <Xml.Serialization.XmlAttribute> Public Property country_iso_code As String
+        <Xml.Serialization.XmlAttribute> Public Property country_name As String
+        <Xml.Serialization.XmlElement("city")> Public Property CityLocations As CityLocation()
+
+        Public Overrides Function ToString() As String
+            Return String.Format("({0}){1}", country_iso_code, country_name)
+        End Function
+    End Class
+
+    Public Class CityLocation
+        <Xml.Serialization.XmlAttribute> Public Property geoname_id As Long
+        <Xml.Serialization.XmlAttribute> Public Property subdivision_1_iso_code As String
+        <Xml.Serialization.XmlAttribute> Public Property subdivision_1_name As String
+        <Xml.Serialization.XmlAttribute> Public Property subdivision_2_iso_code As String
+        <Xml.Serialization.XmlAttribute> Public Property subdivision_2_name As String
+        <Xml.Serialization.XmlAttribute> Public Property city_name As String
+        <Xml.Serialization.XmlAttribute> Public Property metro_code As String
+        <Xml.Serialization.XmlAttribute> Public Property time_zone As String
+        <Xml.Serialization.XmlElement("geoLoci")> Public Property GeographicLocations As GeographicLocation()
+
+        Public Function IPLocatedAtCity(IPAddress As Net.IPAddress) As GeographicLocation
+            If GeographicLocations.IsNullOrEmpty Then
+                Return Nothing
+            End If
+            Dim LQuery = (From Location As GeographicLocation
+                          In Me.GeographicLocations
+                          Where Not Location Is Nothing AndAlso Location.Locating(IPAddress)
+                          Select Location).ToArray
+
+            Return LQuery.FirstOrDefault
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}, {1}/{2}", city_name, subdivision_1_name, subdivision_2_name)
+        End Function
+    End Class
+
+    Public Class GeographicLocation
+
+        Dim _InternalCIDRValue As CIDR
+
+        <Xml.Serialization.XmlAttribute("network")> Public Property CIDR As String
+            Get
+                If _InternalCIDRValue Is Nothing Then
+                    Return ""
+                End If
+                Return _InternalCIDRValue.CIDR
+            End Get
+            Set(value As String)
+                If String.IsNullOrEmpty(value) Then
+                    _InternalCIDRValue = Nothing
+                Else
+                    _InternalCIDRValue = New CIDR(value)
+                End If
+            End Set
+        End Property
+        '<Xml.Serialization.XmlAttribute> Public Property is_anonymous_proxy As Integer
+        '<Xml.Serialization.XmlAttribute> Public Property is_satellite_provider As Integer
+        <Xml.Serialization.XmlAttribute> Public Property postal_code As String
+        <Xml.Serialization.XmlAttribute> Public Property latitude As Double
+        <Xml.Serialization.XmlAttribute> Public Property longitude As Double
+
+        Public Overrides Function ToString() As String
+            Return String.Format("[{0}, {1}]  {2}", latitude, longitude, CIDR)
+        End Function
+
+        ''' <summary>
+        ''' 查看目标IP地址是否落于本地址段之内
+        ''' </summary>
+        ''' <param name="IP"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function Locating(IP As Net.IPAddress) As Boolean
+            If Me._InternalCIDRValue Is Nothing OrElse _InternalCIDRValue.Invalid Then
+                Return False
+            End If
+            Return Me._InternalCIDRValue.Locating(IP)
+        End Function
+
+    End Class
+
+    Public Class CIDR
+
+        Dim StartIP As IPAddress
+        Dim EndIP As IPAddress
+        Dim _InternalMaskBytes As Byte()
+        Dim _CIDR_Mask As String
+
+        Public ReadOnly Property CIDR As String
+            Get
+                Return _CIDR_Mask
+            End Get
+        End Property
+
+        Shared ReadOnly _InternalZEROIP As IPAddress = IPAddress.Parse("0.0.0.0")
+        Shared ReadOnly _Internal255IP As IPAddress = IPAddress.Parse("255.255.255.255")
+
+        Public ReadOnly Property Invalid As Boolean
+            Get
+                Return StartIP.Equals(_InternalZEROIP) OrElse EndIP.Equals(_Internal255IP)
+            End Get
+        End Property
+
+        Sub New(CIDR_Mask As String)
+            Dim CIDR As String() = CIDR_Mask.Split("/"c)
+
+            If CIDR.Length <> 2 Then Throw New Exception(String.Format("Target address value ""{0}"" is not a valid CIDR mask IPAddress value!", CIDR_Mask))
+
+            'obviously some additional error checking would be delightful
+            Dim ip As IPAddress = IPAddress.Parse(CIDR(0))
+            Dim bits As Integer = CInt(CIDR(1))
+            Dim mask As UInteger = Not (UInteger.MaxValue >> bits)
+            Dim ipBytes() As Byte = ip.GetAddressBytes()
+            _InternalMaskBytes = BitConverter.GetBytes(mask).Reverse().ToArray()
+            Dim startIPBytes(ipBytes.Length - 1) As Byte
+            Dim endIPBytes(ipBytes.Length - 1) As Byte
+
+            For i As Integer = 0 To ipBytes.Length - 1
+                startIPBytes(i) = CByte(ipBytes(i) And _InternalMaskBytes(i))
+                endIPBytes(i) = CByte(ipBytes(i) Or (Not _InternalMaskBytes(i)))
+            Next i
+
+            ' You can remove first and last (Network and Broadcast) here if desired
+
+            StartIP = New IPAddress(startIPBytes)
+            EndIP = New IPAddress(endIPBytes)
+            _CIDR_Mask = CIDR_Mask
+
+            If Me.Invalid Then
+                'Call Console.WriteLine(CIDR_Mask & " is not valid!")
+                'StartIP = ip
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' 查看目标IP地址是否落于本地址段之内
+        ''' </summary>
+        ''' <param name="IP"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function Locating(IP As String) As Boolean
+            Return Locating(Net.IPAddress.Parse(IP))
+        End Function
+
+        ''' <summary>
+        ''' 查看目标IP地址是否落于本地址段之内
+        ''' </summary>
+        ''' <param name="IP"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function Locating(IP As Net.IPAddress) As Boolean
+            Dim Tokens = IP.GetAddressBytes
+            Dim Start = StartIP.GetAddressBytes
+            Dim Ends = EndIP.GetAddressBytes
+
+            For i As Integer = 0 To Tokens.Count - 1
+                Dim [byte] As Byte = Tokens(i)
+                If Not ([byte] >= Start(i) AndAlso [byte] <= Ends(i)) Then
+                    Return False
+                End If
+            Next
+
+            Return True
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return StartIP.ToString & " - " & EndIP.ToString & "   [subnet_mask = " &
+                _InternalMaskBytes(0).ToString & "." & _InternalMaskBytes(1).ToString & "." & _InternalMaskBytes(2).ToString & "." & _InternalMaskBytes(3).ToString & "]"
+        End Function
+    End Class
+End Namespace
