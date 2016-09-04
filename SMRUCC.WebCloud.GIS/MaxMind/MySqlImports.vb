5f33d108c76bcc79ad9873fa42a9ec122042ee06
xieguigang <xie.guigang@gmail.com>
2016-09-04 13:20:55 +0800

    finish the mysql imports code

--- SMRUCC.WebCloud.GIS/MaxMind/MySqlImports.vb
+++ SMRUCC.WebCloud.GIS/MaxMind/MySqlImports.vb
@@ -49,15 +49,30 @@ Namespace MaxMind
         ''' 
         <Extension>
         Public Function ImportsGeoLite2CountryBlocksIPv6(mysql As MySQL, df As String) As Boolean
+            Return mysql.ImportsLargeBlock(Of geolite2_country_blocks_ipv6)(df)
+        End Function
+
+        <Extension>
+        Public Function ImportsGeoLite2CityBlocksIPv6(mysql As MySQL, df As String) As Boolean
+            Return mysql.ImportsLargeBlock(Of geolite2_city_blocks_ipv6)(df)
+        End Function
+
+        <Extension>
+        Public Function ImportsGeoLite2CityBlocksIPv4(mysql As MySQL, df As String) As Boolean
+            Return mysql.ImportsLargeBlock(Of geolite2_city_blocks_ipv4)(df)
+        End Function
+
+        <Extension>
+        Public Function ImportsLargeBlock(Of T As SQLTable)(mysql As MySQL, df As String) As Boolean
             Dim SQL As New Value(Of String)
 
             Using reader As New DataStream(df,, 1024 * 1024 * 10)
-                Call mysql.Execute(DropTableSQL(Of geolite2_country_blocks_ipv6))
-                If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of geolite2_country_blocks_ipv6)()) Then
+                Call mysql.Execute(DropTableSQL(Of T))
+                If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of T)()) Then
                     Call mysql.Execute(SQL)
                 End If
 
-                Call reader.ForEach(Of geolite2_country_blocks_ipv6)(AddressOf mysql.ExecInsert)
+                Call reader.ForEach(Of T)(AddressOf mysql.ExecInsert)
             End Using
 
             Return True
@@ -65,15 +80,29 @@ Namespace MaxMind
 
         <Extension>
         Public Function ImportsGeoLite2CountryLocations(mysql As MySQL, DIR As String) As Boolean
+            Return mysql.ImportsLocationFiles(Of geolite2_country_locations)(
+                ls - l - r - wildcards("GeoLite2-Country-Locations*.csv") <= DIR
+            )
+        End Function
+
+        <Extension>
+        Public Function ImportsGeoLite2CityLocations(mysql As MySQL, DIR As String) As Boolean
+            Return mysql.ImportsLocationFiles(Of geolite2_city_locations)(
+                ls - l - r - wildcards("GeoLite2-City-Locations*.csv") <= DIR
+            )
+        End Function
+
+        <Extension>
+        Public Function ImportsLocationFiles(Of T As SQLTable)(mysql As MySQL, files As IEnumerable(Of String)) As Boolean
             Dim SQL As New Value(Of String)
 
-            Call mysql.Execute(DropTableSQL(Of geolite2_country_locations))
-            If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of geolite2_country_locations)()) Then
+            Call mysql.Execute(DropTableSQL(Of T))
+            If Not String.IsNullOrEmpty(SQL = GetCreateTableMetaSQL(Of T)()) Then
                 Call mysql.Execute(SQL)
             End If
 
-            For Each df As String In ls - l - r - wildcards("GeoLite2-Country-Locations*.csv") <= DIR
-                Dim data = df.LoadCsv(Of geolite2_country_locations)
+            For Each df As String In files
+                Dim data = df.LoadCsv(Of T)
                 Dim trans As String = String.Join(vbLf, data.ToArray(Function(x) x.GetInsertSQL))
                 Call mysql.CommitTransaction(trans)
             Next
