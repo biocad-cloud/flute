ffb819ed654137536608a4c7e62d961c79b9ba28
xieguigang <xie.guigang@gmail.com>
2016-10-31 06:51:45 +0800

    scatter data structure

---
+++ SMRUCC.WebCloud.d3js/Scatter.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class serialData
+    Public Property name As String
+    Public Property data As date_count()
+End Class
+
+Public Structure date_count
+    Implements sIdEnumerable
+
+    Public Property [date] As String Implements sIdEnumerable.Identifier
+    Public Property count As Integer
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Structure
