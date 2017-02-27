5b66a1072ef795e4c1ea7800063b710c460c265c
xieguigang <xie.guigang@gmail.com>
2017-02-27 20:31:21 +0800

    add license header

--- HTTP_SERVER/demo/DemoWebApp/ExampleApp.vb
+++ HTTP_SERVER/demo/DemoWebApp/ExampleApp.vb
@@ -1,28 +1,28 @@
-﻿#Region "Microsoft.VisualBasic::492c864bd363d90fd4e5615a588d4511, ..\httpd\HTTP_SERVER\demo\DemoWebApp\ExampleApp.vb"
+﻿#Region "Microsoft.VisualBasic::db3f32e52ae58af643c90f4eb0808455, ..\httpd\HTTP_SERVER\demo\DemoWebApp\ExampleApp.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xieguigang (xie.guigang@live.com)
-'       xie (genetics@smrucc.org)
-' 
-' Copyright (c) 2016 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -116,4 +116,3 @@ Imports SMRUCC.WebCloud.HTTPInternal.Platform
         Return ""
     End Function
 End Class
-
