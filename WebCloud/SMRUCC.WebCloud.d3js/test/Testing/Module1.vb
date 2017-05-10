6967517e8941fad1aecec0d2ba7231e01798b530
xieguigang <xie.guigang@gmail.com>
2017-05-10 22:05:39 +0800

    ??not success on network layout

--- WebCloud/SMRUCC.WebCloud.d3js/test/Testing/Module1.vb
+++ WebCloud/SMRUCC.WebCloud.d3js/test/Testing/Module1.vb
@@ -1,12 +1,15 @@
-﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Data.visualize.Network.Visualize
 
 Module Module1
 
     Sub Main()
-        Dim json = SMRUCC.WebCloud.d3js.Network.htmlwidget.BuildData.BuildGraph("D:\GCModeller\src\runtime\httpd\WebCloud\SMRUCC.WebCloud.d3js\test\viewer.html")
+        Dim json = SMRUCC.WebCloud.d3js.Network.htmlwidget.BuildData.BuildGraph("../../..\viewer.html")
         Dim graph = json.CreateGraph
-        Call graph.doForceLayout
+        Call graph.doForceLayout(showProgress:=True, iterations:=50)
         Call graph.Tabular.Save("./test")
+        Call graph.DrawImage(New Size(2000, 2000), scale:=3).Save("./test.png")
     End Sub
 End Module
