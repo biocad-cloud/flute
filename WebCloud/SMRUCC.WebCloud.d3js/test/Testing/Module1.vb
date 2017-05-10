c2c9a84a49e44aed81d65873d65e64f1d5302f5b
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-10 15:58:53 +0800

    test on layout generator function success

--- WebCloud/SMRUCC.WebCloud.d3js/test/Testing/Module1.vb
+++ WebCloud/SMRUCC.WebCloud.d3js/test/Testing/Module1.vb
@@ -1,7 +1,12 @@
-﻿Module Module1
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+
+Module Module1
 
     Sub Main()
         Dim json = SMRUCC.WebCloud.d3js.Network.htmlwidget.BuildData.BuildGraph("D:\GCModeller\src\runtime\httpd\WebCloud\SMRUCC.WebCloud.d3js\test\viewer.html")
-        Call json .Save ("./test")
+        Dim graph = json.CreateGraph
+        Call graph.doForceLayout
+        Call graph.Tabular.Save("./test")
     End Sub
 End Module
