d74106dfa1deca04028d0292959f0f7cba83775d
xieguigang <xie.guigang@gcmodeller.org>
2017-11-19 02:25:59 +0800

    add vbs extensions

--- WebCloud/SMRUCC.HTTPInternal/Platform/Plugin/Scripting.vb
+++ WebCloud/SMRUCC.HTTPInternal/Platform/Plugin/Scripting.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Serialization.JSON
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
 Namespace Platform.Plugins
@@ -29,5 +30,20 @@ Namespace Platform.Plugins
 
     Public Module ScriptingExtensions
 
+        ReadOnly scripting As IReadOnlyDictionary(Of String, ScriptingAttribute.ScriptHandler)
+
+        Sub New()
+            scripting = App.HOME.LoadHandlers
+        End Sub
+
+        <Extension>
+        Public Function LoadHandlers(dir$) As Dictionary(Of String, ScriptingAttribute.ScriptHandler)
+
+        End Function
+
+        <Extension>
+        Public Function ReadHTML(wwwroot$, path$, Optional encoding As Encodings = Encodings.UTF8) As String
+
+        End Function
     End Module
 End Namespace
