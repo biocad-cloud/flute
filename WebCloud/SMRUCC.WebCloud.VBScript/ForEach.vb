840ccfbf1da49ce14a5df47b1ced65a5533e518c
xieguigang <xie.guigang@gcmodeller.org>
2017-11-19 11:42:36 +0800

    split source code

--- WebCloud/SMRUCC.WebCloud.VBScript/ForEach.vb
+++ WebCloud/SMRUCC.WebCloud.VBScript/ForEach.vb
@@ -15,22 +15,24 @@ Partial Module vbhtml
     Public Function ParseForEach(vbhtml$) As NamedValue(Of String)()
         Dim expression = r _
             .Matches(vbhtml, ForEachLoop, RegexICSng) _
-            .EachValue(Function(input)
-                           Dim value$ = input.GetStackValue("<?", "?>").Trim
-                           Dim t = r _
-                               .Split(value, "(For\s+)|(\s+As\s+)", RegexICSng) _
-                               .Where(Function(s) Not s.StringEmpty) _
-                               .ToArray
-
-                           Return New NamedValue(Of String) With {
-                               .Name = t(0),
-                               .Value = t(1),
-                               .Description = input
-                           }
-                       End Function) _
+            .EachValue(AddressOf parserInternal) _
             .ToArray
 
         Return expression
+    End Function
+
+    Private Function parserInternal(input As String) As NamedValue(Of String)
+        Dim value$ = input.GetStackValue("<?", "?>").Trim
+        Dim t = r _
+            .Split(value, "(For\s+)|(\s+As\s+)", RegexICSng) _
+            .Where(Function(s) Not s.StringEmpty) _
+            .ToArray
+
+        Return New NamedValue(Of String) With {
+            .Name = t(0),
+            .Value = t(1),
+            .Description = input
+        }
     End Function
 
     Public Function Iterates(wwwroot$, vbhtml$, encoding As Encodings) As String
