3af50650a38a23b0e486ec9403b2365f6e87ae7e
xieguigang <xie.guigang@gcmodeller.org>
2017-12-24 17:59:29 +0800

    fix for Dim var as expression

---
+++ WebCloud/SMRUCC.WebCloud.VBScript/DimVar.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports r = System.Text.RegularExpressions.Regex
+
+Partial Module vbhtml
+
+    ''' <summary>
+    ''' ```vbnet
+    ''' Dim $var As &lt;%= path %> 
+    ''' ```
+    ''' </summary>
+    Const DimVar$ = "<\?vb\s+Dim\s+.+?\s+As\s+" & PartialIncludes & "\s+\?>"
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function ParseDimVar(vbhtml$) As NamedValue(Of String)()
+        Dim expression = r _
+            .Matches(vbhtml, DimVar, RegexICSng) _
+            .EachValue(AddressOf parserInternal) _
+            .ToArray
+
+        Return expression
+    End Function
+
+    <Extension>
+    Public Function InterplotDimVar(html As StringBuilder, parent$, args As InterpolateArgs) As StringBuilder
+        Dim expressions = ParseDimVar(html.ToString)
+
+        ' 在这里只生成variable，可能不会进行模板字符串的替换
+        For Each var As NamedValue(Of String) In expressions
+            With var
+                Call args.variables.Add(Mid(.Name, 2), .Value)
+                Call html.Replace(.Description, "")
+            End With
+        Next
+
+        Return html
+    End Function
+End Module
