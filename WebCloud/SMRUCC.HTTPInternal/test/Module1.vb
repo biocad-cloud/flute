15431afc2595d917974ed4ddb39f2a1a5bc624c0
xieguigang <xie.guigang@gcmodeller.org>
2019-06-28 22:18:52 +0800

    config test

--- WebCloud/SMRUCC.HTTPInternal/test/Module1.vb
+++ WebCloud/SMRUCC.HTTPInternal/test/Module1.vb
@@ -1,7 +1,17 @@
-﻿Module Module1
+﻿Imports SMRUCC.WebCloud.HTTPInternal.Core
 
-    Sub Main()
+Module Module1
 
+    Sub Main()  'Program Entry point
+        Dim thread As System.Threading.Thread = New System.Threading.Thread(AddressOf StartWebSocketServer)
+        'Application.Add("WebSocketServerThread", thread) 'Global.asax - context.Application .. I left this part in for web application developers  
+        thread.Start()
+    End Sub
+
+    Public WebSocketServer As WebSocket
+    Public Sub StartWebSocketServer()
+        WebSocketServer = New WebSocket("127.0.0.1", 8000)
+        WebSocketServer.startServer()
     End Sub
 
 End Module
