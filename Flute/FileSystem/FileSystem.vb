636bc8605582c6af27f4be25c2c77122bab8f113
xieguigang <xie.guigang@gcmodeller.org>
2020-01-12 20:54:25 +0800

    fix test for virtual mapped file

--- Flute/FileSystem/FileSystem.vb
+++ Flute/FileSystem/FileSystem.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.FileIO
+Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
 
 ''' <summary>
 ''' Physical file system combine with logical file mapping 
@@ -7,7 +8,7 @@ Public Class FileSystem
 
     Public Property wwwroot As Directory
 
-    ReadOnly virtualMaps As New Dictionary(Of String, Object)
+    ReadOnly virtualMaps As New Dictionary(Of String, FileObject)
 
     ''' <summary>
     ''' Create a new filesystem proxy for http web services
@@ -17,7 +18,48 @@ Public Class FileSystem
         Me.wwwroot = New Directory(directory:=wwwroot)
     End Sub
 
-    Public Function FileExists() As Boolean
+    ''' <summary>
+    ''' 这个函数只适用于小文件的缓存
+    ''' </summary>
+    ''' <param name="resourceUrl$"></param>
+    ''' <param name="file$"></param>
+    ''' <param name="mime"></param>
+    ''' <returns></returns>
+    Public Function AddCache(resourceUrl$, file$, Optional mime As ContentType = Nothing) As FileObject
+        Return AddCache(resourceUrl, file.ReadBinary, mime)
+    End Function
 
+    Public Function AddCache(resourceUrl$, data As Byte(), Optional mime As ContentType = Nothing) As FileObject
+        Dim resource As New MemoryCachedFile(resourceUrl.FileName, data, mime)
+        Dim key$ = FileSystem.resourceUrl(resourceUrl)
+
+        ' add new cache resource or update current 
+        ' existed resource
+        virtualMaps(key) = resource
+
+        Return resource
+    End Function
+
+    Private Shared Function resourceUrl(ByRef pathRelative As String) As String
+        pathRelative = pathRelative.Trim("."c, "/"c, "\"c)
+        Return pathRelative
+    End Function
+
+    Public Function FileExists(pathRelative As String) As Boolean
+        ' test of the physical file at first
+        If resourceUrl(pathRelative).FileExists Then
+            Return True
+        Else
+            ' and then test for the logical file
+            If virtualMaps.ContainsKey(pathRelative) Then
+                If TypeOf virtualMaps(pathRelative) Is VirtualMappedFile Then
+                    Return DirectCast(virtualMaps(pathRelative), VirtualMappedFile).isValid
+                Else
+                    Return True
+                End If
+            End If
+        End If
+
+        Return False
     End Function
 End Class
