ed2ef654ffd4de5e6478c31b892d0b152f1ff810
xieguigang <xie.guigang@gcmodeller.org>
2020-01-12 21:46:15 +0800

    renames namespace

--- Flute/FileSystem/FileSystem.vb
+++ Flute/FileSystem/FileSystem.vb
@@ -4,118 +4,125 @@ Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
 Imports FolderHandle = Microsoft.VisualBasic.FileIO.Directory
 
-''' <summary>
-''' Physical file system combine with logical file mapping 
-''' </summary>
-Public Class FileSystem
-
-    Public Property wwwroot As FolderHandle
-
-    ReadOnly virtualMaps As New Dictionary(Of String, FileObject)
+Namespace FileSystem
 
     ''' <summary>
-    ''' Create a new filesystem proxy for http web services
+    ''' Physical file system combine with logical file mapping 
     ''' </summary>
-    ''' <param name="wwwroot"></param>
-    Sub New(wwwroot As String)
-        Me.wwwroot = New FolderHandle(directory:=wwwroot)
-    End Sub
+    Public Class FileSystem
 
-    ''' <summary>
-    ''' 这个函数只适用于小文件的缓存
-    ''' </summary>
-    ''' <param name="resourceUrl$"></param>
-    ''' <param name="file$"></param>
-    ''' <param name="mime"></param>
-    ''' <returns></returns>
-    Public Function AddCache(resourceUrl$, file$, Optional mime As ContentType = Nothing) As FileObject
-        Return AddCache(resourceUrl, file.ReadBinary, mime)
-    End Function
+        Public ReadOnly Property wwwroot As FolderHandle
 
-    Public Function AddCache(resourceUrl$, data As Byte(), Optional mime As ContentType = Nothing) As FileObject
-        Dim resource As New MemoryCachedFile(resourceUrl.FileName, data, mime)
-        Dim key$ = FileSystem.resourceUrl(resourceUrl)
+        ReadOnly virtualMaps As New Dictionary(Of String, FileObject)
 
-        ' add new cache resource or update current 
-        ' existed resource
-        virtualMaps(key) = resource
+        ''' <summary>
+        ''' Create a new filesystem proxy for http web services
+        ''' </summary>
+        ''' <param name="wwwroot"></param>
+        Sub New(wwwroot As String)
+            Me.wwwroot = New FolderHandle(directory:=wwwroot)
+        End Sub
 
-        Return resource
-    End Function
+        ''' <summary>
+        ''' 这个函数只适用于小文件的缓存
+        ''' </summary>
+        ''' <param name="resourceUrl$"></param>
+        ''' <param name="file$"></param>
+        ''' <param name="mime"></param>
+        ''' <returns></returns>
+        Public Function AddCache(resourceUrl$, file$, Optional mime As ContentType = Nothing) As FileObject
+            Return AddCache(resourceUrl, file.ReadBinary, mime)
+        End Function
 
-    Public Function AddMapping(resourceUrl$, file$, Optional mime As ContentType = Nothing) As FileObject
-        Dim resource As New VirtualMappedFile(resourceUrl.FileName, file, mime)
-        Dim key$ = FileSystem.resourceUrl(resourceUrl)
+        Public Function AddCache(resourceUrl$, data As Byte(), Optional mime As ContentType = Nothing) As FileObject
+            Dim resource As New MemoryCachedFile(resourceUrl.FileName, data, mime)
+            Dim key$ = FileSystem.resourceUrl(resourceUrl)
 
-        ' add new cache resource or update current 
-        ' existed resource
-        virtualMaps(key) = resource
+            ' add new cache resource or update current 
+            ' existed resource
+            virtualMaps(key) = resource
 
-        Return resource
-    End Function
+            Return resource
+        End Function
 
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="directory"></param>
-    ''' <param name="attachTo"></param>
-    ''' <param name="cacheMode">Work in cache mode or mapping mode?</param>
-    ''' <returns></returns>
-    Public Iterator Function AttachFolder(directory$, Optional attachTo$ = "/", Optional cacheMode As Boolean = False) As IEnumerable(Of NamedValue(Of FileObject))
-        Dim resourceUrl$
-        Dim fileObj As FileObject
+        Public Function AddMapping(resourceUrl$, file$, Optional mime As ContentType = Nothing) As FileObject
+            Dim resource As New VirtualMappedFile(resourceUrl.FileName, file, mime)
+            Dim key$ = FileSystem.resourceUrl(resourceUrl)
 
-        For Each file As String In ls - l - r - "*.*" <= directory
-            resourceUrl = attachTo & "/" & RelativePath(directory, file)
+            ' add new cache resource or update current 
+            ' existed resource
+            virtualMaps(key) = resource
 
-            If cacheMode Then
-                fileObj = AddCache(resourceUrl, file)
-            Else
-                fileObj = AddMapping(resourceUrl, file)
-            End If
+            Return resource
+        End Function
 
-            Yield New NamedValue(Of FileObject) With {
-                .Name = resourceUrl,
-                .Description = file,
-                .Value = fileObj
-            }
-        Next
-    End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="directory"></param>
+        ''' <param name="attachTo"></param>
+        ''' <param name="cacheMode">Work in cache mode or mapping mode?</param>
+        ''' <returns></returns>
+        Public Iterator Function AttachFolder(directory$, Optional attachTo$ = "/", Optional cacheMode As Boolean = False) As IEnumerable(Of NamedValue(Of FileObject))
+            Dim resourceUrl$
+            Dim fileObj As FileObject
 
-    Private Shared Function resourceUrl(ByRef pathRelative As String) As String
-        pathRelative = pathRelative.Trim("."c, "/"c, "\"c)
-        Return pathRelative
-    End Function
+            For Each file As String In ls - l - r - "*.*" <= directory
+                resourceUrl = attachTo & "/" & RelativePath(directory, file)
 
-    Public Function GetResource(pathRelative As String) As Stream
-        ' test of the physical file at first
-        If resourceUrl(pathRelative).FileExists Then
-            Return pathRelative.Open(FileMode.Open, doClear:=False)
-        Else
-            ' and then test for the logical file
-            If virtualMaps.ContainsKey(pathRelative) Then
-                Return virtualMaps(pathRelative).GetResource
-            End If
-        End If
-
-        Return New MemoryStream(buffer:={})
-    End Function
-
-    Public Function FileExists(pathRelative As String) As Boolean
-        ' test of the physical file at first
-        If resourceUrl(pathRelative).FileExists Then
-            Return True
-        Else
-            ' and then test for the logical file
-            If virtualMaps.ContainsKey(pathRelative) Then
-                If TypeOf virtualMaps(pathRelative) Is VirtualMappedFile Then
-                    Return DirectCast(virtualMaps(pathRelative), VirtualMappedFile).isValid
+                If cacheMode Then
+                    fileObj = AddCache(resourceUrl, file)
                 Else
-                    Return True
+                    fileObj = AddMapping(resourceUrl, file)
+                End If
+
+                Yield New NamedValue(Of FileObject) With {
+                    .Name = resourceUrl,
+                    .Description = file,
+                    .Value = fileObj
+                }
+            Next
+        End Function
+
+        Private Shared Function resourceUrl(ByRef pathRelative As String) As String
+            pathRelative = pathRelative.Trim("."c, "/"c, "\"c)
+            Return pathRelative
+        End Function
+
+        Public Function GetResource(pathRelative As String) As Stream
+            ' test of the physical file at first
+            If resourceUrl(pathRelative).FileExists Then
+                Return pathRelative.Open(FileMode.Open, doClear:=False)
+            Else
+                ' and then test for the logical file
+                If virtualMaps.ContainsKey(pathRelative) Then
+                    Return virtualMaps(pathRelative).GetResource
                 End If
             End If
-        End If
 
-        Return False
-    End Function
-End Class
+            Return New MemoryStream(buffer:={})
+        End Function
+
+        Public Function FileExists(pathRelative As String) As Boolean
+            ' test of the physical file at first
+            If resourceUrl(pathRelative).FileExists Then
+                Return True
+            Else
+                ' and then test for the logical file
+                If virtualMaps.ContainsKey(pathRelative) Then
+                    If TypeOf virtualMaps(pathRelative) Is VirtualMappedFile Then
+                        Return DirectCast(virtualMaps(pathRelative), VirtualMappedFile).isValid
+                    Else
+                        Return True
+                    End If
+                End If
+            End If
+
+            Return False
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return wwwroot.ToString
+        End Function
+    End Class
+End Namespace
