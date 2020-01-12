5e2a13a2559b8cc37d83495b3726cff94ec63ac0
xieguigang <xie.guigang@gcmodeller.org>
2020-01-12 19:59:08 +0800

    re-design the http file system

---
+++ Flute/FileSystem/FileSystem.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+
+''' <summary>
+''' Physical file system combine with logical file mapping 
+''' </summary>
+Public Class FileSystem
+
+    Public Property wwwroot As Directory
+
+    ''' <summary>
+    ''' Create a new filesystem proxy for http web services
+    ''' </summary>
+    ''' <param name="wwwroot"></param>
+    Sub New(wwwroot As String)
+        Me.wwwroot = New Directory(directory:=wwwroot)
+    End Sub
+End Class
