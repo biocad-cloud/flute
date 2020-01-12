b731e72305d1054908f3b333430fe93c9bd78d3b
xieguigang <xie.guigang@gcmodeller.org>
2020-01-12 20:18:29 +0800

    add file system models

--- Flute/FileSystem/FileSystem.vb
+++ Flute/FileSystem/FileSystem.vb
@@ -7,6 +7,8 @@ Public Class FileSystem
 
     Public Property wwwroot As Directory
 
+    ReadOnly virtualMaps As New Dictionary(Of String, Object)
+
     ''' <summary>
     ''' Create a new filesystem proxy for http web services
     ''' </summary>
@@ -14,4 +16,8 @@ Public Class FileSystem
     Sub New(wwwroot As String)
         Me.wwwroot = New Directory(directory:=wwwroot)
     End Sub
+
+    Public Function FileExists() As Boolean
+
+    End Function
 End Class
