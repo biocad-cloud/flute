f7a1aafaad47caec8c5320fddff3bfde687988c0
xieguigang <xie.guigang@gcmodeller.org>
2021-06-16 00:30:32 +0800

    Update HttpDriver.vb

--- Flute/HttpDriver.vb
+++ Flute/HttpDriver.vb
@@ -1,25 +1,15 @@
 ﻿Imports System.Net.Sockets
 Imports Flute.Http.Core
 
-Public Class HttpDriver : Inherits HttpServer
+Public Class HttpDriver
 
-    Public Sub New(port As Integer, Optional threads As Integer = -1)
-        MyBase.New(port, threads)
+    Dim responseHeader As New Dictionary(Of String, String)
+
+    Sub New()
     End Sub
 
-    Public Overrides Sub handleGETRequest(p As HttpProcessor)
-        Throw New NotImplementedException()
+    Public Sub AddResponseHeader(header As String, value As String)
+        responseHeader.Add(header, value)
     End Sub
 
-    Public Overrides Sub handlePOSTRequest(p As HttpProcessor, inputData As String)
-        Throw New NotImplementedException()
-    End Sub
-
-    Public Overrides Sub handleOtherMethod(p As HttpProcessor)
-        Throw New NotImplementedException()
-    End Sub
-
-    Protected Overrides Function getHttpProcessor(client As TcpClient, bufferSize As Integer) As HttpProcessor
-        Return New HttpProcessor(client, Me, bufferSize)
-    End Function
 End Class
