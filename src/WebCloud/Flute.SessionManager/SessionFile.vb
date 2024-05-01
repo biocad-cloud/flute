edb7d2e6df823311d13c3fb4f17e016f88c12251
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-01 08:22:02 +0800

    create empty file

--- src/WebCloud/Flute.SessionManager/SessionFile.vb
+++ src/WebCloud/Flute.SessionManager/SessionFile.vb
@@ -7,6 +7,10 @@ Public Class SessionFile
 
     Sub New(filepath As String)
         file = filepath
+
+        If Not file.FileExists Then
+            Call New Byte() {}.FlushStream(file)
+        End If
     End Sub
 
     Public Function OpenKey(key As String) As MemoryStream
@@ -19,8 +23,15 @@ Public Class SessionFile
         End If
     End Function
 
+    ''' <summary>
+    ''' [keyname => offset,length,next]
+    ''' </summary>
+    ''' <param name="key"></param>
+    ''' <returns></returns>
     Public Function SearchKey(key As String) As BufferRegion
+        Using s As New BinaryDataReader(New FileStream(file, FileMode.Open))
 
+        End Using
     End Function
 
 End Class
