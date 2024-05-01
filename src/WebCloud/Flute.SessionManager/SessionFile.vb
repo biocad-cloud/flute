82080380e439d4c58a846ad0b2f0ea8cde089d2e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-01 08:28:07 +0800

    open key data

--- src/WebCloud/Flute.SessionManager/SessionFile.vb
+++ src/WebCloud/Flute.SessionManager/SessionFile.vb
@@ -1,4 +1,5 @@
 Imports System.IO
+Imports System.Text
 Imports Microsoft.VisualBasic.Data.IO
 
 Public Class SessionFile
@@ -19,7 +20,14 @@ Public Class SessionFile
         If region Is Nothing Then
             Return Nothing
         Else
+            Using s As New FileStream(file, FileMode.Open)
+                Dim load As Byte() = New Byte(region.size - 1) {}
 
+                Call s.Seek(region.position, SeekOrigin.Begin)
+                Call s.Read(load, Scan0, load.Length)
+
+                Return New MemoryStream(load)
+            End Using
         End If
     End Function
 
@@ -29,8 +37,26 @@ Public Class SessionFile
     ''' <param name="key"></param>
     ''' <returns></returns>
     Public Function SearchKey(key As String) As BufferRegion
-        Using s As New BinaryDataReader(New FileStream(file, FileMode.Open))
+        Using s As New BinaryDataReader(New FileStream(file, FileMode.Open), Encoding.ASCII)
+            For i As Integer = 0 To 100000
+                Dim skey As String = s.ReadString(BinaryStringFormat.ZeroTerminated)
+                Dim start As Long = s.ReadInt64
+                Dim len As Integer = s.ReadInt32
 
+                If skey = key Then
+                    Return New BufferRegion(start, Len)
+                Else
+                    Dim jumpNext As Long = s.ReadInt64
+
+                    Call s.Seek(jumpNext, SeekOrigin.Begin)
+
+                    If s.EndOfStream Then
+                        Exit For
+                    End If
+                End If
+            Next
+
+            Return Nothing
         End Using
     End Function
 
