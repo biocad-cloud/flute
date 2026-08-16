52f9052b43428277fb073853e0f83554aac93ab0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-16 16:10:20 +0800

    split the source file into multiple parts

---
+++ src/Flute/FileSystem/FileObject/MemoryCachedFile.vb
@@ -0,0 +1,31 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
+
+Namespace FileSystem
+
+    Public Class MemoryCachedFile : Inherits FileObject
+
+        ReadOnly cache As MemoryStream
+
+        Public Overrides ReadOnly Property ContentLength As Long
+            Get
+                Return cache.Length
+            End Get
+        End Property
+
+        Sub New(fileName$, data As Byte(), Optional mime As ContentType = Nothing)
+            Call MyBase.New(fileName, mime)
+
+            ' create cache data stream
+            Me.cache = New MemoryStream(data)
+        End Sub
+
+        Public Overrides Function GetResource() As Stream
+            Return cache
+        End Function
+
+        Public Overrides Function GetByteBuffer() As Byte()
+            Return cache.ToArray
+        End Function
+    End Class
+End Namespace
---
+++ src/Flute/FileSystem/FileObject/VirtualMappedFile.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
+
+Namespace FileSystem
+
+    Public Class VirtualMappedFile : Inherits FileObject
+
+        Public ReadOnly Property mappedPath As String
+
+        Public ReadOnly Property isValid As Boolean
+            Get
+                Return mappedPath.FileExists
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property ContentLength As Long
+            Get
+                Return mappedPath.FileLength
+            End Get
+        End Property
+
+        Sub New(fileName$, mappedPath$, Optional mime As ContentType = Nothing)
+            Call MyBase.New(fileName, mime)
+
+            Me.mappedPath = mappedPath
+        End Sub
+
+        Public Overrides Function GetResource() As Stream
+            Return mappedPath.Open(FileMode.Open, doClear:=False)
+        End Function
+
+        Public Overrides Function GetByteBuffer() As Byte()
+            Return mappedPath.ReadBinary
+        End Function
+    End Class
+End Namespace
