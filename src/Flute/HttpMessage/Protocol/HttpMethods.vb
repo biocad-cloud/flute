8f25d8eba15a84aeed6401f75a76310826b89922
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-16 17:00:12 +0800

    Create HttpMethods.vb

---
+++ src/Flute/HttpMessage/Protocol/HttpMethods.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+Namespace Core.Message.HttpHeader
+
+    <AttributeUsage(AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
+    Public Class HttpGet : Inherits ExportAPIAttribute
+
+        Sub New(url As String)
+            Call MyBase.New(url)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"http-get('{Name}')"
+        End Function
+
+    End Class
+
+    <AttributeUsage(AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
+    Public Class HttpPost : Inherits ExportAPIAttribute
+
+        Sub New(url As String)
+            Call MyBase.New(url)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"http-post('{Name}')"
+        End Function
+    End Class
+End Namespace
